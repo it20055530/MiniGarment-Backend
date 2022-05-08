4f2df69ac3927482401a11328265b945b79395d2
Osanda Randunu <it20082888@my.sliit.lk>
2022-05-09 01:07:15 +0530

    all spings repositories

--- src/main/java/com/example/demo/Repository/O_ProductRepository.java
+++ src/main/java/com/example/demo/Repository/O_ProductRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.Repository;
 
-public class O_ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.Model.O_Product;
+
+@Repository
+public interface O_ProductRepository extends JpaRepository<O_Product, Long>{
 
 }
--- src/main/java/com/example/demo/Repository/O_SupplierRepository.java
+++ src/main/java/com/example/demo/Repository/O_SupplierRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.Repository;
 
-public class O_SupplierRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.Model.O_Supplier;
+
+@Repository
+public interface O_SupplierRepository extends JpaRepository<O_Supplier, Long>{
 
 }

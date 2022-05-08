79e67e07c61117f5806988d5a0c4e564d1b6f780
Osanda Randunu <it20082888@my.sliit.lk>
2022-05-09 01:09:50 +0530

    all control classes for spings

--- src/main/java/com/example/demo/Controller/O_ProductController.java
+++ src/main/java/com/example/demo/Controller/O_ProductController.java
@@ -1,5 +1,66 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Exception.ResourceNotFoundException;
+import com.example.demo.Model.O_Product;
+import com.example.demo.Repository.O_ProductRepository;
+
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
 public class O_ProductController {
 
+	@Autowired
+	private O_ProductRepository o_ProductRepository;
+	
+		//all Products
+		@GetMapping("/products")
+		public List<O_Product> getAllSuppliers(){
+			return o_ProductRepository.findAll();
+		}
+		
+		//create product
+		@PostMapping("/products")
+		public O_Product createProduct(@RequestBody O_Product product) {
+			return o_ProductRepository.save(product);
+		}
+		
+		//get employee by id
+		@GetMapping("/products/{id}")
+		public ResponseEntity<O_Product> getProductById(@PathVariable Long pid) {
+			O_Product product = o_ProductRepository.findById(pid).orElseThrow(() -> new ResourceNotFoundException("Product is not existed with this ID : "+pid));
+			
+			return ResponseEntity.ok(product);
+		}
+		
+		//Update supplier
+		@PutMapping("/products/{id}")
+		public ResponseEntity<O_Product> updateProduct(@PathVariable Long pid,@RequestBody O_Product productdetails){
+			O_Product product = o_ProductRepository.findById(pid).orElseThrow(() -> new ResourceNotFoundException("Product is not existed with this ID : "+pid));
+			
+			product.setProduct(productdetails.getProduct());
+			product.setMaterialType(productdetails.getMaterialType());
+			product.setSize(productdetails.getSize());
+			product.setColor(productdetails.getColor());
+			product.setFeatures(productdetails.getFeatures());
+			product.setType(productdetails.getType());
+			product.setQuantity(productdetails.getQuantity());
+			
+			O_Product updateproduct = o_ProductRepository.save(product);
+			return ResponseEntity.ok(updateproduct);
+		}
 }
--- src/main/java/com/example/demo/Controller/O_SupplierController.java
+++ src/main/java/com/example/demo/Controller/O_SupplierController.java
@@ -1,5 +1,66 @@
 package com.example.demo.Controller;
 
-public class O_SupplierController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Exception.ResourceNotFoundException;
+import com.example.demo.Model.O_Supplier;
+import com.example.demo.Repository.O_SupplierRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class O_SupplierController {
+	
+	@Autowired
+	private O_SupplierRepository o_SupplierRepository;
+
+	//all suppliers
+	@GetMapping("/suppliers")
+	public List<O_Supplier> getAllSuppliers(){
+		return o_SupplierRepository.findAll();
+	}
+	
+	//create supplier
+	@PostMapping("/suppliers")
+	public O_Supplier createSupplier(@RequestBody O_Supplier supplier) {
+		return o_SupplierRepository.save(supplier);
+	}
+	
+	//get employee by id
+	@GetMapping("/suppliers/{id}")
+	public ResponseEntity<O_Supplier> getSupplierById(@PathVariable Long id) {
+		O_Supplier supplier = o_SupplierRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Supplier not existed with this ID : "+id));
+		
+		return ResponseEntity.ok(supplier);
+	}
+	
+	//Update supplier
+	@PutMapping("/suppliers/{id}")
+	public ResponseEntity<O_Supplier> updateSupplier(@PathVariable Long id,@RequestBody O_Supplier supplierdetails){
+		 
+		O_Supplier supplier = o_SupplierRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Supplier not existed with this ID : "+id));
+		
+		supplier.setSname(supplierdetails.getSname());
+		supplier.setSnic(supplierdetails.getSnic());
+		supplier.setStype(supplierdetails.getStype());
+		supplier.setScompany(supplierdetails.getScompany());
+		supplier.setSemail(supplierdetails.getSemail());
+		supplier.setSphone(supplierdetails.getSphone());
+		supplier.setScapacity(supplierdetails.getScapacity());
+		
+		O_Supplier updatesupplier = o_SupplierRepository.save(supplier);
+		return ResponseEntity.ok(updatesupplier);
+		
+	}
 }

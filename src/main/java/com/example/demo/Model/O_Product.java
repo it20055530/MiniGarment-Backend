1284a5e942ab35ee36f9912c2da5c41bea2fbe04
Osanda Randunu <it20082888@my.sliit.lk>
2022-05-09 00:57:09 +0530

    model of spring 2 back end

--- src/main/java/com/example/demo/Model/O_Product.java
+++ src/main/java/com/example/demo/Model/O_Product.java
@@ -1,5 +1,117 @@
-package com.example.demo.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class O_Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long pid;
+	
+	@Column(name = "Product")
+	private String Product;
+	
+	@Column(name = "materialtype")
+	private String MaterialType;
+	
+	@Column(name = "size")
+	private String Size;
+	
+	@Column(name = "color")
+	private String Color;
+	
+	@Column(name = "features")
+	private String Features;
+	
+	@Column(name = "type")
+	private String Type;
+	
+	@Column(name = "quantity")
+	private String Quantity;
+	
+	public O_Product() {
+		
+	}
+
+	public O_Product(String product, String materialType, String size, String color, String features, String type,
+			String quantity) {
+		super();
+		Product = product;
+		MaterialType = materialType;
+		Size = size;
+		Color = color;
+		Features = features;
+		Type = type;
+		Quantity = quantity;
+	}
+
+	public long getPid() {
+		return pid;
+	}
+
+	public void setPid(long pid) {
+		this.pid = pid;
+	}
+
+	public String getProduct() {
+		return Product;
+	}
+
+	public void setProduct(String product) {
+		Product = product;
+	}
+
+	public String getMaterialType() {
+		return MaterialType;
+	}
+
+	public void setMaterialType(String materialType) {
+		MaterialType = materialType;
+	}
+
+	public String getSize() {
+		return Size;
+	}
+
+	public void setSize(String size) {
+		Size = size;
+	}
+
+	public String getColor() {
+		return Color;
+	}
+
+	public void setColor(String color) {
+		Color = color;
+	}
+
+	public String getFeatures() {
+		return Features;
+	}
+
+	public void setFeatures(String features) {
+		Features = features;
+	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public String getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		Quantity = quantity;
+	}
 
 }
--- src/main/java/com/example/demo/Model/O_Supplier.java
+++ src/main/java/com/example/demo/Model/O_Supplier.java
@@ -1,5 +1,102 @@
-package com.example.demo.Model;
+  package com.example.demo.Model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "suppliers")
 public class O_Supplier {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String Sname;
+	
+	@Column(name = "nic")
+	private String Snic;
+	
+	@Column(name = "type")
+	private String Stype;
+	
+	@Column(name = "company")
+	private String Scompany;
+	
+	@Column(name = "email")
+	private String Semail;
+	
+	@Column(name = "phone")
+	private String Sphone;
+	
+	@Column(name = "capacity")
+	private String Scapacity;
+	
+	public O_Supplier() {
+		
+	}
+	
+	public O_Supplier(String sname, String snic, String stype, String scompany, String semail, String sphone,String scapacity) {
+		super();
+		Sname = sname;
+		Snic = snic;
+		Stype = stype;
+		Scompany = scompany;
+		Semail = semail;
+		Sphone = sphone;
+		Scapacity = scapacity;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getSname() {
+		return Sname;
+	}
+	public void setSname(String sname) {
+		Sname = sname;
+	}
+	public String getSnic() {
+		return Snic;
+	}
+	public void setSnic(String snic) {
+		Snic = snic;
+	}
+	public String getStype() {
+		return Stype;
+	}
+	public void setStype(String stype) {
+		Stype = stype;
+	}
+	public String getScompany() {
+		return Scompany;
+	}
+	public void setScompany(String scompany) {
+		Scompany = scompany;
+	}
+	public String getSemail() {
+		return Semail;
+	}
+	public void setSemail(String semail) {
+		Semail = semail;
+	}
+	public String getSphone() {
+		return Sphone;
+	}
+	public void setSphone(String sphone) {
+		Sphone = sphone;
+	}
+	public String getScapacity() {
+		return Scapacity;
+	}
+	public void setScapacity(String scapacity) {
+		Scapacity = scapacity;
+	}
 }
+
